fbf762f725793156ee1cc103016278ebe0ba9271
Bayarbaatar Bayandalai <47892101+probayraka@users.noreply.github.com>
2020-12-01 10:25:40 +0800

    Updates

---
+++ precache-manifest.4988279660df67d66766ae7edaa0a1f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4fd920a7c6449d5371e4ce8124b523",
+    "url": "/ubn_productino_12.01/index.html"
+  },
+  {
+    "revision": "e5c165f6e10b410df199",
+    "url": "/ubn_productino_12.01/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "14a13c09ab93b05ae7ca",
+    "url": "/ubn_productino_12.01/static/js/2.53601c60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ubn_productino_12.01/static/js/2.53601c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c165f6e10b410df199",
+    "url": "/ubn_productino_12.01/static/js/main.3d9530ee.chunk.js"
+  },
+  {
+    "revision": "3e3e8fcb5b02f786a8fe",
+    "url": "/ubn_productino_12.01/static/js/runtime-main.3942dc52.js"
+  }
+]);
